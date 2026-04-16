6052611c62afb122d9d134d5450c2eadfea81ac0
asyiffaurrohman <akungithub934@gmail.com>
2026-04-16 15:56:02 +0700

    Menambahkan bubblesort array

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -5,3 +5,32 @@ int element[10];
 int nPanjang;
 int x;
 
+void input()
+{
+    while (true)
+    {
+        cout << "masukkan banyaknya elemen pada array (maksimal 10) :";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. silakan coba lagi.\n";
+        }
+    }
+
+    cout << "==============================\n";
+    cout << "   Masukkan elemen array      \n";
+    cout << "==============================\n";
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Elemen ke-" << (i + 1) << " = ";
+        cin >> element[i];
+    }
+}
+
+void BubbleSortArray()
